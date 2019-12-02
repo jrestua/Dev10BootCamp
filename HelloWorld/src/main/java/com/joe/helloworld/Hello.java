2d6d59a641ba8aec820f6e2be7556889e00220bb
“jrestua” <jtr9@njit.edu>
2019-12-02 15:36:58 -0500

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/joe/helloworld/Hello.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Joseph Restua
+ */
+package com.joe.helloworld; // Use YOUR Group Id value here instead of com.tsg
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Joe!");
+    }
+}
