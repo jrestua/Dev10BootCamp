831bf9602d4b3c217feffa7c0a5224fed7bc7594
“jrestua” <jtr9@njit.edu>
2019-12-09 14:10:43 -0500

    Organized Folders

--- HelloWorld/src/main/java/AssessmentBasicProgrammingConcepts/RockPaperScissor.java
+++ HelloWorld/src/main/java/AssessmentBasicProgrammingConcepts/RockPaperScissor.java
@@ -16,31 +16,57 @@ public class RockPaperScissor {
     
     public static void main(String[] args){
         
+        Scanner myScanner = new Scanner(System.in);
         
-        int numRounds = rounds();
+        //Declaring and Initializing Variables \
         int userWins = 0;
         int computerWins = 0;
         int ties = 0;
+        String playAgain = "y";
         
-               
+        //while loop to start game, also continues in loop if the user wants to play again.
+        while(playAgain.equals("y")){
+        //Calls for number of rounds user wants to play
+        int numRounds = rounds();
+        //For loop to loop through the code depending on how many rounds user types in.       
         for(int i = 0; i < numRounds; ++i){
             int rockPaperOrScissors = userInput();
             int computerInputChoice = randomInput();
             
-            if(rockPaperOrScissors > computerInputChoice){
+            //You win if your choice is greater than the computers.
+            if((rockPaperOrScissors == 1 ) && (computerInputChoice == 3) ){
                 System.out.println("You won this round!");
                 userWins++;
             }
-            if(computerInputChoice > rockPaperOrScissors){
+            //You lose if your choice is less than the computers.
+            if((rockPaperOrScissors == 1 ) && (computerInputChoice == 2) ){
                 System.out.println("The computer won this round!");
                 computerWins++;
             }
+            if((rockPaperOrScissors == 2 ) && (computerInputChoice == 1) ){
+                System.out.println("You won this round!");
+                computerWins++;
+            }
+            if((rockPaperOrScissors == 2 ) && (computerInputChoice == 3) ){
+                System.out.println("The computer won this round!");
+                computerWins++;
+            }
+             if((rockPaperOrScissors == 3 ) && (computerInputChoice == 2) ){
+                System.out.println("You won this round!");
+                computerWins++;
+            }
+            if((rockPaperOrScissors == 3 ) && (computerInputChoice == 1) ){
+                System.out.println("The computer won this round!");
+                computerWins++;
+            }
+            //Tie when both choices equal eachother.
             if(computerInputChoice == rockPaperOrScissors){
                 System.out.println("It's a tie.");
                 ties++;
             }
         }
         
+        //Print summary
         System.out.println("Your win total: " + userWins);
         System.out.println("Computer win total: " + computerWins);
         System.out.println("Tie toal: " + ties);
@@ -56,11 +82,16 @@ public class RockPaperScissor {
             System.out.println("The game ended in a tie.");
             System.out.println("");
             }
-        
-        
+        //Ask if you would like to play again.
+        System.out.println("Would you like to play again? y/n: ");
+        playAgain = myScanner.nextLine();
+        if (playAgain.equals("n")){
+            System.out.println("Thank you for playing!");
+        }
+    }
     }
     
-    
+    //Method asking for number of rounds.
     public static int rounds(){
         Scanner myScanner = new Scanner(System.in);
         int rounds;
@@ -80,7 +111,7 @@ public class RockPaperScissor {
     }
     
     
-    
+    //Method asking for user input. Rock, paper, or scissor.
     public static int userInput(){
         Scanner myScanner = new Scanner(System.in);
         
@@ -110,6 +141,7 @@ public class RockPaperScissor {
         
         } 
     
+    //Creates random input for computer.
     public static int randomInput(){
         Random rand = new Random();
         int randomInteger = rand.nextInt((3 - 1) + 1) + 1;;
